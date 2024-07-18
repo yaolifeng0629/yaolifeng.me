947c042de70f328661a49627eb343e08b269efe1
Immerse <y15829485647@163.com>
2024-07-18 17:53:37 +0800

    feat: init home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
-import Button from '@/components/button';
+import { HeroSection } from '@/pages/home';
 
 export default function Home() {
-    const router = useRouter();
-
-    function toZustand() {
-        router.push('/zustand_use_demo');
-    }
-    function toAPI() {
-        router.push('/test_request');
-    }
-    function toAntd() {
-        router.push('/antd_use');
-    }
-    function toVitest() {
-        router.push('/test_vitest');
-    }
-
     return (
         <div className="w-full h-full flex justify-center items-center flex-col">
-            <h1 className="text-4xl font-bold">Next.js Project Template</h1>
-            <div className="flex mt-10">
-                <Button text={'Zustand Use'} click={toZustand} />
-                <Button text={'API Request Use'} click={toAPI} />
-                <Button text={'Antd Use'} click={toAntd} />
-                <Button text={'Vitest'} click={toVitest} />
-            </div>
+            <HeroSection />
         </div>
     );
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export * from "./ascii";
+
+export * from "./info";
+
+export * from "./path";
+
+export * from "./regex";
+
+// export * from "./unknown";
+
+export * from "./pagination";
+
+export * from "./error";
+
+
+// export * from "./assets";
+
+// export * from "./meilisearch";
