b5e8d5c4a6306bb71cd8ddebe87e373bbe2bb96e
Immerse <y15829485647@163.com>
2024-07-22 23:32:53 +0800

    feat: add part component entry

---
+++ src/components/icons/logos/icon-logo-weixin.tsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// import { cn } from '@/lib/utils';
+
+// export const IconLogoWeChat = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
+//     return <span {...props} className={cn('icon-[logos--wechat]', className)}></span>;
+// };
