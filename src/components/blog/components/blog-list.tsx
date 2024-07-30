993da94b76df7e50e977880d9718c1102ed8bb47
Immerse <y15829485647@163.com>
2024-07-30 18:04:45 +0800

    feat: blog list

--- src/components/blog/components/blog-list.tsx
+++ src/components/blog/components/blog-list.tsx
@@ -1,40 +1,39 @@
-// import { Key } from 'react';
+import { Key } from 'react';
 
-// import { IllustrationNoContent } from '@/components/illustrations';
+import { IllustrationNoContent } from '@/components/illustrations';
 
-// import { type Blog } from '../types';
+import { type Blog } from '../types';
 
-// import { BlogListItem } from './blog-list-item';
+import { BlogListItem } from './blog-list-item';
 
-// type BlogListProps = {
-//     blog: never;
-// };
+type BlogListProps = {
+    blog: never;
+};
 
-export const BlogList = () => {
-    // if (!blogs.length) {
-    //     return (
-    //         <div className="grid place-content-center gap-8">
-    //             <IllustrationNoContent className="size-[30vh]" />
-    //             <h3 className="text-center text-2xl font-semibold tracking-tight">暂无Blog</h3>
-    //         </div>
-    //     );
-    // }
+export const BlogList = ({ blogs }) => {
+    if (!blogs.length) {
+        return (
+            <div className="grid place-content-center gap-8">
+                <IllustrationNoContent className="size-[30vh]" />
+                <h3 className="text-center text-2xl font-semibold tracking-tight">暂无Blog</h3>
+            </div>
+        );
+    }
 
-    // console.log('blogs ---->', blogs);
+    console.log('blogs ---->', blogs);
     return (
-        <div>blog list</div>
-        // <ul className="grid grid-cols-1 gap-10 md:grid-cols-2">
-        //     {blogs.map((el: { id: Key | null | undefined }, idx: number) => (
-        //         <li
-        //             key={el.id}
-        //             className="animate-fade-up animate-ease-in-out"
-        //             style={{
-        //                 animationDelay: `${(idx + 1) * 200}ms`
-        //             }}
-        //         >
-        //             <BlogListItem blog={el} uvMap={uvMap} />
-        //         </li>
-        //     ))}
-        // </ul>
+        <ul className="grid grid-cols-1 gap-10 md:grid-cols-2">
+            {blogs.map((el: { id: Key | null | undefined }, idx: number) => (
+                <li
+                    key={el.id}
+                    className="animate-fade-up animate-ease-in-out"
+                    style={{
+                        animationDelay: `${(idx + 1) * 200}ms`
+                    }}
+                >
+                    <BlogListItem blog={el} uvMap={0} />
+                </li>
+            ))}
+        </ul>
     );
 };
