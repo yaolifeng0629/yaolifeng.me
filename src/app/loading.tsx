12e0a1865f3716dbc9a38a7b583ff700acf72df5
yaolifeng <y15829485647@163.com>
2024-10-16 11:37:29 +0800

    perf: opt style

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,5 +2,15 @@ import React from 'react';
 import './loading.css';
 
 export default function Loading() {
-    return <div className="loading"></div>;
+    return <div className='min-h-[calc(100vh-190px)] flex justify-center items-center'>
+        <div className="cssload-container">
+            <ul className="cssload-flex-container">
+                <li>
+                    <span className="cssload-loading cssload-one"></span>
+                    <span className="cssload-loading cssload-two"></span>
+                    <span className="cssload-loading-center"></span>
+                </li>
+            </ul>
+        </div>
+    </div >
 }
