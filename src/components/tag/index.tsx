ecd313b7a39416a7c6bf5fdcb81d6ee75bfd12aa
Immerse <y15829485647@163.com>
2024-10-19 22:40:56 +0800

    perf: opt tag text color

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -6,7 +6,7 @@ interface TagProps {
 
 const Tag: React.FC<TagProps> = ({ text }) => {
     return (
-        <span className="inline-block bg-[#18181880] text-gray-500 text-xs font-medium px-2.5 py-0.5 rounded border border-[#1f1f1f] overflow ">
+        <span className="inline-block bg-[#18181880] text-gray-400 text-xs font-medium px-2.5 py-0.5 rounded border border-[#1f1f1f] overflow ">
             #&nbsp;{text}
         </span>
     );
