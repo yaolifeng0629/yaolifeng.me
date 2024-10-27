c72447a7571634743a8826bd0df6e9c4bac19c55
Immerse <y15829485647@163.com>
2024-10-27 17:31:40 +0800

    style: navbar border bottom

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
         <header
             className={cn(
                 'w-full sticky top-0 backdrop-blur transition-[background-color,border-width] border-x-0  flex justify-center z-10',
-                (scroll?.top ?? 0) > 60 && 'bg-background/50 border-b border-border/50',
+                (scroll?.top ?? 0) > 60 && 'bg-background/50 border-b border-[#232329]',
             )}
         >
             <div className="flex h-16 w-full justify-between items-center p-4 sm:p-8 md:max-w-screen-md 2xl:max-w-screen-xl">
